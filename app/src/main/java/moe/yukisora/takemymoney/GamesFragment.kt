a0e2bc7a5389b1289a998ad0860a6a4af7faf2ea
yukisora <yukisora2013@gmail.com>
2017-08-28 22:36:52 +0800

    view pager

---
+++ app/src/main/java/moe/yukisora/takemymoney/GamesFragment.kt
@@ -0,0 +1,27 @@
+package moe.yukisora.takemymoney
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GamesFragment : Fragment() {
+    companion object {
+        fun newInstance(): GamesFragment {
+            val args = Bundle()
+            val fragment = GamesFragment()
+            fragment.arguments = args
+
+            return fragment
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_games, container, false)
+    }
+}
--- app/src/main/java/moe/yukisora/takemymoney/MainActivity.kt
+++ app/src/main/java/moe/yukisora/takemymoney/MainActivity.kt
@@ -1,12 +1,35 @@
 package moe.yukisora.takemymoney
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val PAGER_COUNT = 3
+    }
+    private lateinit var viewPager:ViewPager;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.viewPager)
+        viewPager.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment? {
+                return when(position) {
+                    0 -> SpecialFragment.newInstance()
+                    1 -> GamesFragment.newInstance()
+                    2 -> SettingFragment.newInstance()
+                    else -> null
+                }
+            }
+
+            override fun getCount(): Int {
+                return PAGER_COUNT
+            }
+        }
     }
 }
