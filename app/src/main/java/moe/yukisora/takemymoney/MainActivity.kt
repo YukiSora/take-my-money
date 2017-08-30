1de911bd7ba3c5666f4f8bdd463de8aca2db1f98
yukisora <yukisora2013@gmail.com>
2017-08-30 19:38:30 +0800

    bottom navigation bar

--- app/src/main/java/moe/yukisora/takemymoney/MainActivity.kt
+++ app/src/main/java/moe/yukisora/takemymoney/MainActivity.kt
@@ -1,6 +1,7 @@
 package moe.yukisora.takemymoney
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v4.view.ViewPager
@@ -11,21 +12,25 @@ class MainActivity : AppCompatActivity() {
     companion object {
         const val PAGER_COUNT = 3
     }
+
     private lateinit var viewPager: ViewPager;
     private lateinit var toolbar: Toolbar;
+    private lateinit var bottomNavigationView: BottomNavigationView;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         toolbar = findViewById(R.id.toolbar)
+        viewPager = findViewById(R.id.viewPager)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
         setSupportActionBar(toolbar)
         toolbar.title = "Special"
 
-        viewPager = findViewById(R.id.viewPager)
         viewPager.adapter = object : FragmentPagerAdapter(supportFragmentManager) {
             override fun getItem(position: Int): Fragment {
-                return when(position) {
+                return when (position) {
                     0 -> SpecialFragment.newInstance()
                     1 -> GamesFragment.newInstance()
                     else -> SettingFragment.newInstance()
@@ -36,7 +41,6 @@ class MainActivity : AppCompatActivity() {
                 return PAGER_COUNT
             }
         }
-
         viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrollStateChanged(state: Int) {
             }
@@ -45,12 +49,39 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onPageSelected(position: Int) {
-                when(position) {
-                    0 -> toolbar.title = "Special"
-                    1 -> toolbar.title = "Games"
-                    2 -> toolbar.title = "Setting"
+                when (position) {
+                    0 -> {
+                        toolbar.title = "Special"
+                        bottomNavigationView.selectedItemId = R.id.special_item
+                    }
+                    1 -> {
+                        toolbar.title = "Games"
+                        bottomNavigationView.selectedItemId = R.id.games_item
+                    }
+                    2 -> {
+                        toolbar.title = "Setting"
+                        bottomNavigationView.selectedItemId = R.id.setting_item
+                    }
                 }
             }
         })
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.special_item -> {
+                    viewPager.setCurrentItem(0, true)
+                    true
+                }
+                R.id.games_item -> {
+                    viewPager.setCurrentItem(1, true)
+                    true
+                }
+                R.id.setting_item -> {
+                    viewPager.setCurrentItem(2, true)
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
