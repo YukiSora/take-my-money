dcd5a2ed0ededc374de52bc7bcb671f0998532b5
yukisora <yukisora2013@gmail.com>
2017-09-06 20:24:44 +0800

    reload image

--- app/src/main/java/moe/yukisora/takemymoney/adapters/SpecialRecyclerViewAdapter.kt
+++ app/src/main/java/moe/yukisora/takemymoney/adapters/SpecialRecyclerViewAdapter.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
 import moe.yukisora.takemymoney.R
 import moe.yukisora.takemymoney.models.SpecialModel
@@ -22,21 +23,33 @@ class SpecialRecyclerViewAdapter(private val context: Context, private val speci
 
     override fun getItemCount(): Int = specials.size
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         private val logo: ImageView = view.findViewById(R.id.logo)
         private val name: TextView = view.findViewById(R.id.name)
         private val discount: TextView = view.findViewById(R.id.discount)
         private val price: TextView = view.findViewById(R.id.price)
 
-        internal fun bindData(special: SpecialModel) {
-            Picasso.with(context)
-                    .load(special.logoUrl)
-                    .placeholder(R.drawable.animation_progress)
-                    .error(R.drawable.bitmap_error)
-                    .into(logo)
+        fun bindData(special: SpecialModel) {
+            loadImage(special)
             name.text = special.name
             discount.text = special.discount
             price.text = special.price
+        }
+
+        private fun loadImage(special: SpecialModel) {
+            Picasso.with(context)
+                    .load(special.logoUrl)
+                    .placeholder(R.drawable.animation_progress)
+                    .error(R.drawable.bitmap_reload)
+                    .into(logo, object : Callback {
+                        override fun onSuccess() {
+                            logo.isClickable = false
+                        }
+
+                        override fun onError() {
+                            logo.setOnClickListener { loadImage(special) }
+                        }
+                    })
         }
     }
 }
